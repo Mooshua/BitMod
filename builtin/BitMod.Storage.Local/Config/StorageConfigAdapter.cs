b8e2f4ba7c3963ce537b7fafbc899d5cdcc3ed0f
Mooshua <43320783+mooshua@users.noreply.github.com>
2023-11-04 13:08:10 -0700

    First draft of local storage driver

---
+++ builtin/BitMod.Storage.Local/Config/StorageConfigAdapter.cs
@@ -0,0 +1,21 @@
+﻿using BitMod.Configuration.Model;
+
+namespace BitMod.Storage.Local.Config;
+
+public class StorageConfigAdapter
+{
+	private const string PATH = "path";
+
+	private IConfigObject _configObject;
+
+	public StorageConfigAdapter(IConfigObject configObject)
+	{
+		_configObject = configObject;
+	}
+
+	public bool HasPath()
+		=> _configObject.Get<IConfigSymbol>(PATH) != null;
+
+	public string? Path => _configObject.Get<IConfigSymbol>(PATH)?.Symbol;
+
+}
---
+++ builtin/BitMod.Storage.Local/Host/StorageHost.cs
@@ -0,0 +1,51 @@
+﻿
+using BattleBitAPI.Common;
+
+using BitMod.Attributes.Injects;
+using BitMod.Attributes.Targets;
+using BitMod.Configuration.Model;
+using BitMod.Events.Stats;
+using BitMod.Plugins.Events;
+using BitMod.Storage.Local.Config;
+
+namespace BitMod.Storage.Local.Host;
+
+public class StorageHost
+{
+
+	[Config("storage_local")]
+	private IConfigObject _configObject;
+
+	private StorageConfigAdapter _config => new StorageConfigAdapter(_configObject);
+
+	private string Build(string id)
+	{
+		if (!_config.HasPath() || _config.Path == null)
+			throw new InvalidOperationException("local storage config does not have a valid path!");
+
+		Directory.CreateDirectory(_config.Path);
+		return Path.Combine(_config.Path, id);
+	}
+
+	[BitProducer]
+	public async Task<Product> OnGetStats(GetPlayerStatsEventArgs ev)
+	{
+		var path = Build(ev.SteamID.ToString());
+
+		if (!File.Exists(path))
+			return Product.None();
+
+		var bytes = File.ReadAllBytes(path);
+
+		return Product.Produce(new PlayerStats(bytes));
+	}
+
+	[BitEvent]
+	public async Task OnSaveStats(SavingPlayerStatsEventArgs ev)
+	{
+		var path = Build(ev.SteamID.ToString());
+
+		File.WriteAllBytes(path, ev.PlayerStats.SerializeToByteArray());
+	}
+
+}
